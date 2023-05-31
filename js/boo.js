16232b247a77d7eff49aa6acd6d5f3a95ec6d843
Jeong woo jin <wjddnwls77@ewhain.net>
2023-05-31 12:23:53 +0900

    start

---
+++ js/boo.js
@@ -0,0 +1,19 @@
+window.onload =function(){
+    let stationSlider = new Swiper(".station ",{
+     
+        pagination: {
+            el: '.swiper-pagination',
+    },
+    autoplay:{
+        delay:2000,
+        // pauseOnMouseEnter:true
+    },
+
+
+
+})
+
+
+
+
+}
